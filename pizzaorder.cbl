ad10d53cb357773f39a7926d898d515ff167d76d
JDSwan <jeffdswan@gmail.com>
2019-07-25 21:07:05 -0500

    Initial commit. Created program file with descriptive comment.

---
+++ pizzaorder.cbl
@@ -0,0 +1,28 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PIZZAORDER.
+
+      ******************************************************************
+      * This program is a COBOL implementation of the pizza ordering   *
+      * program suggested by Samuel Fare in his Medium post about      *
+      * learning programming languages.                                *
+      * https://medium.com/@samuel.fare/want-to-learn-any-programming- *
+      * language-write-these-3-simple-apps-5af8cd119921                *
+      ******************************************************************
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+
+       PROCEDURE DIVISION.
+
+       9000-END-PROGRAM.
+           STOP RUN.
+
+       END PROGRAM PIZZAORDER.
